51079940e6bce394ba9d2149d719516bc53bf77a
The1024thmember <1786728683@qq.com>
2021-11-09 18:29:02 +1100

    popUp message normal now

---
+++ chatapp/app/helpers/popupmessage.js
@@ -0,0 +1,11 @@
+export function popUpMessage(message) {
+    //14 chars, what exceeds 14 chars will change into ...
+    let newMessage = JSON.parse(JSON.stringify(message));
+    newMessage = newMessage[0];
+    if (newMessage.length > 14) {
+      newMessage = newMessage.slice(0,12)+"...";
+    }
+    return newMessage;
+  }
+  
+  export default Ember.Helper.helper(popUpMessage);
