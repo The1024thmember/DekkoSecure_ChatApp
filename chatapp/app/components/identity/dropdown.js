62df18394bb457351e0f07ed8c673acac6110282
The1024thmember <1786728683@qq.com>
2021-11-21 13:25:47 +1100

    track chooseuse

--- chatapp/app/components/identity/dropdown.js
+++ chatapp/app/components/identity/dropdown.js
@@ -9,7 +9,7 @@ export default class DropDownComponent extends Component {
 
   //get the user's identity
   @action
-  choosePeople(event) {
+  async choosePeople(event) {
     this.people = event.target.innerHTML;
     this.confirmPeople.getIdentity(event.target.innerHTML);
 
@@ -29,5 +29,21 @@ export default class DropDownComponent extends Component {
       }
     });
     this.confirmPeople.getOtherPeople(contacts);
+
+
+    //send hupspot choosepeople
+    let settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'text/plain',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "Identifier":"xiaoCai",
+        "Event name":"pe21033607_chooseuser"
+      })
+    };
+    let response = await fetch('https://api/hubspot.com/events/v3/send', settings);
+    let data = await response.json();
   }
 }
